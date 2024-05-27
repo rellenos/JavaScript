9f1fe6147c505076a6947554bbb848c6a5ec68e1
rellenos <danieling00@gmail.com>
2024-05-27 13:57:28 +0200

    fin de clase

--- functions.js
+++ functions.js
@@ -10,11 +10,9 @@ function sum(x,y) {
 function calcul(operador1,operador2,operante) {
     if (operante == "+")
         resultat = operador1 + operador2;
-    else
-        if (operante == "-")
+    else if (operante == "-")
             resultat = operador1 - operador2;
-        else
-            if (operante == "*")
+        else if (operante == "*")
                 resultat = operador1 * operador2;
             else
                 resultat = operador1 / operador2;
