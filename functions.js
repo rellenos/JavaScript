62b06a2780fb398bb1d2e5aeb43c499a03b2e0f1
rellenos <danieling00@gmail.com>
2024-05-27 13:15:16 +0200

    operación matemática

--- functions.js
+++ functions.js
@@ -2,3 +2,21 @@ let name = "Daniel";
 let surname = "Mansilla";
 let age = 24;
 let pet = false;
+
+function sum(x,y) {
+    return x + y;
+}
+
+function calcul(operador1,operador2,operante) {
+    if (operante == "+")
+        resultat = operador1 + operador2;
+    else
+        if (operante == "-")
+            resultat = operador1 - operador2;
+        else
+            if (operante == "*")
+                resultat = operador1 * operador2;
+            else
+                resultat = operador1 / operador2;
+    return resultat;
+}
