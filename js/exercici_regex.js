4ea3704a5fa62c379e24eb5390c15fac4e95189f
rellenos <danieling00@gmail.com>
2024-06-11 12:39:04 +0200

    final ejercicios regex

--- js/exercici_regex.js
+++ js/exercici_regex.js
@@ -24,20 +24,77 @@ function esTargetaCreditValida(tarjeta) {
 }
 
 function extraureHashtags(hashtags) {
-    patro = /\#(\w+|\d+)/g;
+    patro = /#(\w|\d)+/g;
     return hashtags.match(patro);
 }
 
-// function esPasswordValida(contrasenya) {
-//     patro = /^([A-Z]+)([a-z]+)([0-9]+)([#?!@$%^&*- ]+)/;
-//     return patro.test(contrasenya);
-// }
+function esPasswordValida(contrasenya) {
+    patro = /[A-Za-z0-9#?!@$%^&*-]+/;
+    patro = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+    return patro.test(contrasenya);
+}
 
 function extraureDates(data) {
     patro = /(\d{2}[/]){2}\d{4}/g;
     return data.match(patro);
 }
 
-// function treureParaulesDuplicades(duplicat) {
+function treureParaulesDuplicades(duplicat) {
+    patro = /\b(\w+)\b(?:\s+\1\b)+/g;
+    return duplicat.replace(patro,'$1')
+}
 
-// }
+function esURLValida(link) {
+    patro = /(http|https|ftp)[:][/]{2}(\w+[.|/]){2,}\w+/;
+    return patro.test(link);
+}
+
+function esSegSocValida(numss) {
+    patro = /^\d{3}[-]\d{2}[-]\d{4}$/;
+    return patro.test(numss);
+}
+
+function extraureUsuarisTwitter(usuari) {
+    patro = /\@(\d+|\w+|_)/g;
+    return usuari.match(patro);
+}
+
+function esEAN13Valid(codi) {
+    patro = /^\d{13}$/;
+    return patro.test(codi);
+}
+
+function extractURLs(url) {
+    patro = /(https|http|ftp)[:][/]{2}\w+[.](\w+|\d+)[.]\w+/g;
+    return url.match(patro);
+}
+
+function esFitxerValid(fitxer) {
+    patro = /(\w|\d)*[.]\w{1,3}$/;
+    return patro.test(fitxer);
+}
+
+function extraureColorsHex(color) {
+    patro = /[#](\b(\w|\d){3}\b|\b(\w|\d){6}\b)/g;
+    return color.match(patro);
+}
+
+function esISBN10Valid(isbn) {
+    patro = /\b(\d{9}X?)[^0]\b/;
+    return patro.test(isbn);
+}
+
+function extraureParaulesMajuscula(mayus) {
+    patro = /[A-Z]\w+/g;
+    return mayus.match(patro);
+}
+
+function esMatriculaValida(matricula) {
+    patro = /\b[A-Z]{3}[-]\d+\b/;
+    return patro.test(matricula);
+}
+
+function extraurePreus(preu) {
+    patro = /[$]\d+[.]\d{2}\b/g;
+    return preu.match(patro);
+}
